a3baf2d8660c0c40c663c51336921789e5bf9229
jluke-russell <jluke.russell@gmail.com>
2023-05-12 12:42:23 -0700

    added stuff

--- Housing/housing.py
+++ Housing/housing.py
@@ -1,6 +1,19 @@
 # %%
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
 housing = pd.read_csv('https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/housing.csv')
 housing.info()
+# %%
+import seaborn as sns
+# correlation matrix helps us see what features are related and by how much. 
+corr_matrix = housing.corr()
+# visualize the correlation matrix
+fig, ax = plt.subplots(figsize=(20, 18))
+hm = sns.heatmap(corr_matrix, annot=True)
+hm.set_title("Correlation Heatmap of Housing Data")
+plt.show()
+
+
 # %%
