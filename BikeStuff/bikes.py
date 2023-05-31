7ec8e20bf158f91b0cbb2efe709db1bc628c7298
jluke-russell <jluke.russell@gmail.com>
2023-05-31 13:40:20 -0600

    new files data dict and bike

--- BikeStuff/bikes.py
+++ BikeStuff/bikes.py
@@ -1,1 +1,13 @@
+# %%
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import tensorflow as tf
+from keras.models import Sequential 
+from keras.layers import Dense
+
+
+bikes = pd.read_csv("https://raw.githubusercontent.com/byui-cse/cse450-course/master/data/bikes.csv")
+
+bikes.head()
+
+# %%
